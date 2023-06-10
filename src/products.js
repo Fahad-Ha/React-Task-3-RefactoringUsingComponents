404d3a25d7965525119ac3deba99b4b59362c6bf
Fahad <fahhod.ahmad@gmail.com>
2023-06-10 07:35:49 +0300

    cleaned the App.js

--- src/products.js
+++ src/products.js
@@ -1,24 +1,17 @@
-const products = [
+export const products = [
   {
     id: 1,
-    name: "Chocolate Chip Cookie",
-    price: 10,
+    name: "Bookl1",
+    price: "5.00 KD",
     image:
-      "https://joyfoodsunshine.com/wp-content/uploads/2016/01/best-chocolate-chip-cookies-recipe-ever-no-chilling-1.jpg",
+      "https://miblart.com/wp-content/uploads/2020/01/crime-and-mystery-cover-scaled-1.jpeg",
   },
   {
     id: 2,
-    name: "Cute Cookie",
-    price: 15,
+    name: "Bookl2",
+    price: "5.00 KD",
     image:
-      "https://i.pinimg.com/originals/f6/3e/2a/f63e2a1cd0c7d3c0ab9cd277d3f32050.jpg",
-  },
-  {
-    id: 3,
-    name: "Katakeet Cookie",
-    price: 7,
-    image:
-      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fassets.marthastewart.com%2Fstyles%2Fwmax-750%2Fd34%2Feaster-chick-egg-cookies-102921707%2Feaster-chick-egg-cookies-102921707_horiz.jpg%3Fitok%3DUBZfwNLI",
+      "https://www.designbookcover.pt/uploads/media/photos/cache/the_pen_and_the_sword_72_media_huge_thumbnail.jpg",
   },
 ];
 
